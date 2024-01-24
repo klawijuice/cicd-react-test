2896f5d25faeb9747d03c2d287a2b933e3512458
Ange Romeo <romeo.gouebi@devolution.africa>
2024-01-24 11:02:36 +0000

    next commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'prettier',
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.js'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        'prettier/prettier': 'error',
+    },
+    settings: {react: {version: 'detect'}},
 }

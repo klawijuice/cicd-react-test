8dc239a8d83ea4e536b1f3f5a2c1b81a4d0317b5
Ange Romeo <romeo.gouebi@devolution.africa>
2024-01-24 11:39:50 +0000

    Next new commit 1 hello world for commit test.

---
+++ commitlint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    extends: [],
+    rules: {
+        'header-min-length': [2, 'always', 20],
+        'header-case-start-capital': [2, 'always'],
+        'header-end-period': [2, 'always'],
+    },
+    plugins: [
+        {
+            rules: {
+                'header-case-start-capital': ({raw}) => {
+                    return [
+                        /^[A-Z]/.test(raw),
+                        'Commit message header must start with a capital letter.'
+                    ]
+                },
+                'header-end-period': ({raw}) => {
+                    return [
+                        /\.$/.test(raw),
+                        'Commit message header must end with a period.'
+                    ]
+                }
+            }
+        }
+    ]
+}
